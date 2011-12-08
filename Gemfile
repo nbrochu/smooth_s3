3cf696873ae06621df551d8e2eb5c5e5fd9587f9
Nicholas Brochu <info@nicholasbrochu.com>
2011-12-08 12:14:48 -0500

    Getting ready to package as a gem

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 
 gem "s3", "~> 0.3.9"
+gem "jeweler"
--- lib/smooth_s3.rb
+++ lib/smooth_s3.rb
@@ -1,12 +1,14 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__)) unless $LOAD_PATH.include?(File.dirname(__FILE__))
+
 require 'rubygems'
 require 's3'
 
 require 'find'
 
-require './lib/smooth_s3/service.rb'
-require './lib/smooth_s3/bucket.rb'
-require './lib/smooth_s3/uploader.rb'
-require './lib/smooth_s3/error.rb'
+require 'smooth_s3/service.rb'
+require 'smooth_s3/bucket.rb'
+require 'smooth_s3/uploader.rb'
+require 'smooth_s3/error.rb'
 
 module SmoothS3
   VERSION = "0.1.0"
